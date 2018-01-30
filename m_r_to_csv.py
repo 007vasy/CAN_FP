2b14996791c5197773b1372039879040067d2f97
vasy <007vasy@gmail.com>
2018-01-30 09:29:09 +0100

    matlab to csv observation reduction phase

--- m_r_to_csv.py
+++ m_r_to_csv.py
@@ -3,6 +3,7 @@ import scipy.io
 import pandas as pd
 import os
 import glob
+import math
 
 os.chdir("/home/vassb/fingerprint_data/")
 export_location="/home/vassb/fingerprint_data/ansgar_att_six_forklift/"
@@ -13,12 +14,22 @@ wd_filenames = glob.glob('*.mat')
 #   temp_dict=scipy.io.loadmat(file)
 #   for name,dict_ in temp_dict.items():
 #       print name
-
+output_df = pd.DataFrame()
+flag = True
 temp_dict=scipy.io.loadmat('Part 01 Daimler F 00150 MultiTimeChannel.mat')
 for name,dict_ in temp_dict.items():
     print name
     #temp_df.head()
     #print dict_
     temp_df = pd.DataFrame(dict_)
+    temp_df.iloc[0] = math.floor(temp_df.iloc[0])
+    temp_df.names = ['time_ID_s',name]
+    temp_df.groupby(by='time_ID_s', as_index=False)[[name]].mean()
     print temp_df.head()
     print temp_df.tail()
+    if(flag):
+        output_df = temp_df
+        flag = False
+    else:
+        output_df.join(temp_df, how='outer')
+    print output_df.head()

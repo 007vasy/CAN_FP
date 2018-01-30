b972bd439b24ba829fc59fb94f30326306eb99d3
vasy <007vasy@gmail.com>
2018-01-30 11:10:24 +0100

    matlab file reducer user friendly

--- m_r_to_csv.py
+++ m_r_to_csv.py
@@ -12,11 +12,12 @@ export_location="/home/vassb/fingerprint_data/ansgar_att_six_forklift/"
 wd_filenames = glob.glob('*.mat')
 
 for file in wd_filenames:
+    print file
     output_df = pd.DataFrame()
     flag = True
     temp_dict=scipy.io.loadmat(file)
     for name,dict_ in temp_dict.items():
-        print name
+        #print name
         #temp_df.head()
         #print dict_
         temp_df = pd.DataFrame(dict_, columns=['time_ID_s',str(name)])
@@ -31,5 +32,6 @@ for file in wd_filenames:
         else:
             output_df = output_df.merge(temp_df, how='outer', on = 'time_ID_s')
         # print output_df.columns
-    filemane, file_extesions = os.path.splitext(file)
+    filename, file_extesions = os.path.splitext(file)
     output_df.to_csv(export_location +'_'+ filename +'.csv',index=False)
+    print file + ' converted and reduced to ' + filename +'.csv'

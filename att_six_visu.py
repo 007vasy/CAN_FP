1338631004d441384aafec2a4402017bade16e8d
vasy <007vasy@gmail.com>
2018-02-06 09:02:30 +0100

    visu skeleton, hist3D in progress

--- att_six_visu.py
+++ att_six_visu.py
@@ -1,7 +1,7 @@
 # six forklift CSV visualisation with bokeh
 
-
-import scipy.io
+from mpl_toolkits.mplot3d import Axes3D as Axes3D
+import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import os.path
@@ -48,4 +48,26 @@ for file in file_list:
         #     "Travelling_", gsub(".csv", "", file, fixed=TRUE), "_wl_wol_", wl_wol, "_3Dplot.png", sep=""))
         # png(filename=paste(export_location, "Travelling_", gsub(".csv", "", file, fixed=TRUE), "_wl_wol_", wl_wol,
         #                    "_3Dplot.png", sep=""))
-    
+
+fig = plt.figure()
+ax = Axes3D(fig)
+x, y = np.random.rand(2, 100) * 4
+hist, xedges, yedges = np.histogram2d(x, y, bins=4, range=[[0, 4], [0, 4]])
+
+# Construct arrays for the anchor positions of the 16 bars.
+# Note: np.meshgrid gives arrays in (ny, nx) so we use 'F' to flatten xpos,
+# ypos in column-major order. For numpy >= 1.7, we could instead call meshgrid
+# with indexing='ij'.
+xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
+xpos = xpos.flatten('F')
+ypos = ypos.flatten('F')
+zpos = np.zeros_like(xpos)
+
+# Construct arrays with the dimensions for the 16 bars.
+dx = 0.5 * np.ones_like(zpos)
+dy = dx.copy()
+dz = hist.flatten()
+
+ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='g', zsort='average')
+
+plt.show()

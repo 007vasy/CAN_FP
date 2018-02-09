660a94e1888a8e2407e7de5f519b72530ce52bc5
vasy <007vasy@gmail.com>
2018-02-09 11:37:46 +0100

    3d hist data factory

--- att_six_visu.py
+++ att_six_visu.py
@@ -8,13 +8,6 @@ import os.path
 import glob
 import numpy as np
 
-def Steering_Cat(x):
-    if abs(x) <= 30:
-        return 1
-    elif abs(x) <= 60:
-        return 2
-    else:
-        return 3
 
 os.chdir("/home/vassb/fingerprint_data/ansgar_att_six_forklift_att_merged/")
 export_location = "/home/vassb/fingerprint_data/ansgar_att_six_forklift_att_merged/"
@@ -29,46 +22,45 @@ for file in file_list:
 
     for wl_wol in [0, 1]:
         for s_type in range(1,max(temp_df["steering_cat"])):
+            #Dynamic
             plot_df = temp_df.query('steering_cat == s_type and is.weight == wl_wol')
+            x = plot_df["Speed_Drivemotor_1_RPM"]
+            y = plot_df["Torque_Drivemotor_1_Nm"]
+            hist, xedges, yedges = np.histogram2d(x, y, bins=8, range=[
+                [min(temp_df["Speed_Drivemotor_1_RPM"]), max(temp_df["Speed_Drivemotor_1_RPM"])],
+                [min(temp_df["Torque_Drivemotor_1_Nm"]), max(temp_df["Torque_Drivemotor_1_Nm"])]])
 
-            # hist3D_fancy(
-            #     x=plot_df$Speed_Drivemotor_1_RPM, y = plot_df$Torque_Drivemotor_1_Nm, xlab = "RPM", ylab = "Nm", breaks = 8, main = paste(
-            #     "Dynamic_", gsub(".csv", "", file, fixed=TRUE), "_s_type:", s_type, "_wl_wol_", wl_wol, "_3Dplot.png", sep=""))
-            # png(filename=paste(export_location, "Dynamic_", gsub(".csv", "", file, fixed=TRUE), "_s_type:", s_type, "_wl_wol_",
-            #                    wl_wol, "_3Dplot.png", sep=""))
-            # }
-        plot_df = temp_df.query('steering_cat == s_type')
+            m = np.asmatrix(np.rot90(hist))
+            result = np.zeros((9, 9))
+            result[:m.shape[0], :m.shape[1]] = m
+
+            pd.DataFrame(result, index=[str(n) for n in yedges[::-1]], columns=[str(n) for n in xedges]).to_csv(
+                export_location +"Dynamic_"+ os.path.splitext(file)[0] +"_s_type:"+ s_type+ "_wl_wol_"+ wl_wol+ "_3Dplot.csv")
+
+        #Stress_
+        # plot_df = temp_df.query('steering_cat == s_type')
+        # x = plot_df["date_time"]
+        # y = plot_df["Torque_Drivemotor_1_Nm"]
+        # hist, xedges, yedges = np.histogram2d(x, y, bins=8, range=[
+        #     [min(temp_df["Speed_Drivemotor_1_RPM"]), max(temp_df["Speed_Drivemotor_1_RPM"])],
+        #     [min(temp_df["Torque_Drivemotor_1_Nm"]), max(temp_df["Torque_Drivemotor_1_Nm"])]])
+        #
+        # m = np.asmatrix(np.rot90(hist))
+        # result = np.zeros((9, 9))
+        # result[:m.shape[0], :m.shape[1]] = m
+        #
+        # pd.DataFrame(result, index=[str(n) for n in yedges[::-1]], columns=[str(n) for n in xedges]).to_csv(
+        #     export_location + "Stress_" + os.path.splitext(file)[0] + "_wl_wol_" + wl_wol + "_3Dplot.csv")
         # hist3D_fancy(x=as.numeric(
         #     plot_df$date_time), y = plot_df$Torque_Drivemotor_1_Nm, breaks = 8, xlab = "RPM", ylab = "Nm", breaks = 8, main = paste(
         #     "Stress_", gsub(".csv", "", file, fixed=TRUE), "_wl_wol_", wl_wol, "_3Dplot.png", sep=""))
         # png(filename=paste(export_location, "Stress_", gsub(".csv", "", file, fixed=TRUE), "_wl_wol_", wl_wol,
         #                    "_3Dplot.png", sep=""))
 
+        #Travelling_
+
         # hist3D_fancy(x=as.numeric(
         #     plot_df$date_time), y = plot_df$Speed_Drivemotor_1_RPM, breaks = 8, xlab = "RPM", ylab = "Nm", breaks = 8, main = paste(
         #     "Travelling_", gsub(".csv", "", file, fixed=TRUE), "_wl_wol_", wl_wol, "_3Dplot.png", sep=""))
         # png(filename=paste(export_location, "Travelling_", gsub(".csv", "", file, fixed=TRUE), "_wl_wol_", wl_wol,
         #                    "_3Dplot.png", sep=""))
-
-fig = plt.figure()
-ax = Axes3D(fig)
-x, y = np.random.rand(2, 100) * 4
-hist, xedges, yedges = np.histogram2d(x, y, bins=4, range=[[0, 4], [0, 4]])
-
-# Construct arrays for the anchor positions of the 16 bars.
-# Note: np.meshgrid gives arrays in (ny, nx) so we use 'F' to flatten xpos,
-# ypos in column-major order. For numpy >= 1.7, we could instead call meshgrid
-# with indexing='ij'.
-xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
-xpos = xpos.flatten('F')
-ypos = ypos.flatten('F')
-zpos = np.zeros_like(xpos)
-
-# Construct arrays with the dimensions for the 16 bars.
-dx = 0.5 * np.ones_like(zpos)
-dy = dx.copy()
-dz = hist.flatten()
-
-ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='g', zsort='average')
-
-plt.show()
--- att_visu_test.py
+++ att_visu_test.py
@@ -8,44 +8,50 @@ import os.path
 import glob
 import numpy as np
 
-def Steering_Cat(x):
-    if (abs(x) <= 30).bool():
-        return 1
-    elif (abs(x) <= 60).bool():
-        return 2
-    else:
-        return 3
+
 
 #os.chdir("/home/vassb/fingerprint_data/ansgar_att_six_forklift_att_merged/")
 #export_location = "/home/vassb/fingerprint_data/ansgar_att_six_forklift_att_merged/"
 
 
 temp_df = pd.DataFrame(pd.read_csv("/media/vasy/Data/Doksik/projekts/AITIA/can_fp_att_merged/IMRl_F_00214_att_merged.csv"))
-# NEEDED?
-temp_df = temp_df.dropna(how='any')
-temp_df[["steering_cat"]] = temp_df[["Steering_angle_angle"]].apply(np.vectorize(Steering_Cat))
 
+temp_df = temp_df.dropna(how='any')
 
 fig = plt.figure()
 ax = Axes3D(fig)
+
 x = temp_df["Speed_Drivemotor_1_RPM"]
 y = temp_df["Torque_Drivemotor_1_Nm"]
-hist, xedges, yedges = np.histogram2d(x, y, bins=8, range=[[min(temp_df[["Speed_Drivemotor_1_RPM"]]), max(temp_df[["Speed_Drivemotor_1_RPM"]])], [temp_df[["Torque_Drivemotor_1_Nm"]], temp_df[["Torque_Drivemotor_1_Nm"]]]])
+hist, xedges, yedges = np.histogram2d(x, y, bins=8, range=[[min(temp_df["Speed_Drivemotor_1_RPM"]), max(temp_df["Speed_Drivemotor_1_RPM"])], [min(temp_df["Torque_Drivemotor_1_Nm"]), max(temp_df["Torque_Drivemotor_1_Nm"])]])
 
-# Construct arrays for the anchor positions of the 16 bars.
+
+
 # Note: np.meshgrid gives arrays in (ny, nx) so we use 'F' to flatten xpos,
 # ypos in column-major order. For numpy >= 1.7, we could instead call meshgrid
 # with indexing='ij'.
-xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25)
+xpos, ypos = np.meshgrid(xedges[:-1], yedges[:-1])
 xpos = xpos.flatten('F')
 ypos = ypos.flatten('F')
 zpos = np.zeros_like(xpos)
 
 # Construct arrays with the dimensions for the 16 bars.
-dx = 0.5 * np.ones_like(zpos)
-dy = dx.copy()
+dx = 5 * np.ones_like(xpos)
+#dy = dx.copy()
+dy = 5 * np.ones_like(ypos)
 dz = hist.flatten()
 
 ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='g', zsort='average')
 
 plt.show()
+
+m= np.asmatrix(np.rot90(hist))
+result = np.zeros((9,9))
+result[:m.shape[0],:m.shape[1]] = m
+
+pd.DataFrame(result,index=[str(n) for n in yedges[::-1]],columns=[str(n) for n in xedges]).to_csv("foo6.csv")
+
+result = np.zeros((9,9))
+m = np.asmatrix([[1 , 2],[3,4]])
+
+result[:m.shape[0],:m.shape[1]] = m
